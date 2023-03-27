b25138a126f3aa1f634a5ce0d8feae5b1fce2cd0
Matthew <mattjinks@gmail.com>
2023-03-27 14:38:06 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import SelectComponent from "./SelectComponent";
+import "./styles.css";
 
-function App() {
-  return (
+const options = [
+  { key: 1, value: "Test 1" },
+  { key: 2, value: "Test 2" },
+  { key: 3, value: "Test 3" },
+  { key: 4, value: "Test 4" }
+];
+export default function App() {
+    const [selectedPickup, setSelectedPickup] = useState("");
+    const [selectedDropoff, setSelectedDropoff] = useState("");
+    return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Uber/Lyft Price Estimator</h1>
+
+        <div className="pickup-selector">
+        <SelectComponent
+            options={options}
+            onChange={(item) => setSelectedPickup(item)}
+            selectedKey={selectedPickup}
+            placeholder={"Pickup"}
+        />
+        </div>
+
+        <div className="dropoff">
+        <SelectComponent
+            options={options}
+            onChange={(item) => setSelectedDropoff(item)}
+            selectedKey={selectedDropoff}
+            placeholder={"Dropoff"}
+        />
+        </div>
+
+        
     </div>
   );
 }
-
-export default App;
